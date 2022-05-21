11676f05dd19fc5b3660d3056a4b66d047d973ea
Alexis Hayat <azen0x.alexis@gmail.com>
2022-05-21 18:25:04 +0200

    feat: first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { App } = require("@slack/bolt");
 require("dotenv").config();
 // Initializes your app with your bot token and signing secret
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
+    token: process.env.SLACK_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
